7aa21225b49f546675699624534f3bc2a38e550f
Eon Viljoen <Eon.Viljoen@dariel.co.za>
2024-01-12 13:45:14 +0200

    changed structure of secrets

--- models/Secret.cs
+++ models/Secret.cs
@@ -3,4 +3,6 @@ namespace Spotify_Playlist_Sync.models.Secret;
 public class Secret {
     public string ClientId {get; set;}
     public string SecretId {get; set;}
+    public string AccessTokenEndpoint {get; set;}
+    public string AuthorizeTokenEndpoint {get; set;}
 }
